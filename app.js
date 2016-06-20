42f8beabd46c25358e67b9eb3ee3f20e56e7a277
Justin <justin.chau623@gmail.com>
2016-06-19 22:54:54 -0700

    - fill autocomplete box with ticker information - get historical stock data from yahoo (csv)

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-$.ajaxSetup({'cache': true});
-
 function initAutoComplete(){
     $("input#stocklookup").autocomplete({
         source: function(request, response){
             $.ajax({
                 url: '//d.yimg.com/autoc.finance.yahoo.com/autoc?query='+request.term+'&region=US&lang=en-US',
-                //cache: true,
                 dataType: 'jsonp',
-                jsonpCallback: 'YAHOO.util.ScriptNodeDataSource.callbacks',
-                jsonp: 'callback',
-                headers: {
-                    'Cache-Control': 'max-age=0'
-                }
+                jsonpCallback: 'YAHOO.util.ScriptNodeDataSource.callbacks'
             });
 
             YAHOO = {
@@ -19,10 +12,27 @@ function initAutoComplete(){
                     ScriptNodeDataSource: {
                         callbacks: function(data) {
                             console.log(data);
+                            response($.map(data.ResultSet.Result, function(ticker){
+                                return {
+                                    label: ticker.symbol + " - " + ticker.name + " (" + ticker.exchDisp + ")",
+                                    value: ticker.symbol
+                                }
+                            }))
                         }
                     }
                 }
             };
+        },
+        select: function(event, ui){
+            $.ajax({
+                url: ' https://ichart.yahoo.com/table.csv?s=AAPL&a=5&b=19&c=2015&d=5&e=19&f=2016',
+                dataType: 'jsonp',
+                jsonpCallback: 'getCSV'
+            });
+            var getCSV = function(data) {
+                console.log(data)
+            };
+            console.log(ui.item.value);
         }
     })
 }
