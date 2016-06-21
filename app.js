b212b2855d70bc7bf5e3dbfdc4bf6c702fab6a4d
Justin <justin.chau623@gmail.com>
2016-06-20 22:44:37 -0700

    query builder function and switch to YQL

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function initAutoComplete(){
                 dataType: 'jsonp',
                 jsonpCallback: 'YAHOO.util.ScriptNodeDataSource.callbacks'
             });
-
             YAHOO = {
                 util: {
                     ScriptNodeDataSource: {
@@ -25,14 +24,44 @@ function initAutoComplete(){
         },
         select: function(event, ui){
             $.ajax({
-                url: ' https://ichart.yahoo.com/table.csv?s=AAPL&a=5&b=19&c=2015&d=5&e=19&f=2016',
-                dataType: 'jsonp',
-                jsonpCallback: 'getCSV'
+                url: queryBuilder(ui.item.value, 'json', false, 'getJSON'),
+                dataType: 'jsonp'
             });
-            var getCSV = function(data) {
-                console.log(data)
-            };
             console.log(ui.item.value);
         }
     })
+}
+
+/*
+YQL finance query builder
+YQL turns CSV from ichart.yahoo.com/table.csv?s=... to JSON
+extra step necessary because CSV can't be returned via JSONP
+ */
+function queryBuilder(symbol, type, diagnostics, callback){
+    var resource;
+    var dateRange = dateBuilder();
+    resource = 'http://query.yahooapis.com/v1/public/yql?q=' +
+            'select * from   yahoo.finance.historicaldata ' +
+            'where           symbol = "' + symbol + '"' +
+            'and    startDate = "' + dateRange[0] + '"' +
+            'and    endDate   = "' + dateRange[1] + '"' +
+            '&format=' + type  +
+            '&diagnostics=' + diagnostics.toString() +
+            '&env=store://datatables.org/alltableswithkeys' + '&callback=' + callback;
+    console.log(resource);
+    return resource;
+
+    function dateBuilder(){
+        var now = new Date();
+        var day = now.getDate() < 10 ? '0' + now.getDate().toString() : now.getDate().toString();
+        var month = now.getMonth()+1 < 10 ? '0' + (now.getMonth()+1).toString() : (now.getMonth()+1).toString();
+        var year = now.getFullYear();
+        return [(year-1).toString() + '-' + month + '-' + day,
+                year.toString() + '-' + month + '-' + day];
+
+    }
+}
+
+function getJSON(data) {
+    console.log(data);
 }
