71900806d231cf1e1f36404044f2e9068d90b99f
Justin <justin.chau623@gmail.com>
2016-07-07 22:20:38 -0700

    abandoning project: added check to see if stock was eligible by sending three requests yahoo rate limited me so no thanks

--- app.js
+++ app.js
@@ -28,20 +28,49 @@ function initAutoComplete(){
         select: function(event, ui){
             var stock = ui.item;
             //check if has been trading in first and last week of range
+            //first week:
             $.ajax({
                 url: queryBuilder(stock.ticker, 'json', false, 'getJSON',
-                    (function(){
-                    var start = new Date();
-                    start.setDate(now.getDate() - 7);
-                    return start;
-                    }),
-                    new Date()),
+                    (function () {
+                        var start = new Date();
+                        start.setFullYear(start.getFullYear() - 1);
+                        return start;
+                    }()),
+                    (function () {
+                        var end = new Date();
+                        end.setFullYear(end.getFullYear() - 1);
+                        end.setDate(end.getDate() - 7);
+                        return end;
+                    }())),
                 dataType: 'jsonp'
             });
 
+            //last week:
+            $.ajax({
+                url: queryBuilder(stock.ticker, 'json', false, 'getJSON',
+                    (function(){
+                        var start = new Date();
+                        start.setDate(start.getDate()-7);
+                        return start;
+                    }()),
+                    (function(){
+                        return new Date();
+                    }())),
+                dataType: 'jsonp'
+            });
+
+
             //get full data
             $.ajax({
-                url: queryBuilder(stock.ticker, 'json', false, 'getJSON'),
+                url: queryBuilder(stock.ticker, 'json', false, 'getJSON',
+                    (function(){
+                        var start = new Date();
+                        start.setFullYear(start.getFullYear() - 1);
+                        return start;
+                    }()),
+                    (function(){
+                        return new Date();
+                    }())),
                 dataType: 'jsonp'
             });
 
@@ -61,7 +90,7 @@ extra step necessary because CSV can't be returned via JSONP
  */
 function queryBuilder(symbol, type, diagnostics, callback, start, end){
     var resource;
-    var dateRange = dateBuilder(new Date());
+    var dateRange = dateBuilder(start, end);
     resource = 'http://query.yahooapis.com/v1/public/yql?q=' +
             'select * from   yahoo.finance.historicaldata ' +
             'where           symbol = "' + symbol + '"' +
@@ -74,12 +103,25 @@ function queryBuilder(symbol, type, diagnostics, callback, start, end){
     return resource;
 
     //build date in yyyy-mm-dd format
-    function dateBuilder(now){
-        var day = now.getDate() < 10 ? '0' + now.getDate().toString() : now.getDate().toString();
+    function dateBuilder(start, end){
+        console.log(start);
+        var sDay = start.getDate() < 10 ? '0' + start.getDate().toString() : start.getDate().toString();
+        var sMonth = start.getMonth() + 1 < 10 ? '0' + (start.getMonth()+1).toString() : (start.getMonth()+1).toString();
+        var sYear = start.getFullYear().toString();
+
+        var eDay = end.getDate() < 10 ? '0' + end.getDate().toString() : end.getDate().toString();
+        var eMonth = end.getMonth() + 1 < 10 ? '0' + (end.getMonth()+1).toString() : (end.getMonth()+1).toString();
+        var eYear = end.getFullYear().toString();
+
+        return [sYear + '-' + sMonth + '-' + sDay,
+                eYear + '-' + eMonth + '-' + eDay];
+
+
+     /*   var day = now.getDate() < 10 ? '0' + now.getDate().toString() : now.getDate().toString();
         var month = now.getMonth()+1 < 10 ? '0' + (now.getMonth()+1).toString() : (now.getMonth()+1).toString();
         var year = now.getFullYear();
         return [(year-1).toString() + '-' + month + '-' + day,
-                year.toString() + '-' + month + '-' + day];
+                year.toString() + '-' + month + '-' + day];*/
 
     }
 }
